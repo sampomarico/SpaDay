4a2980a6a1881506b7a1029e23220473bcbc564c
Sam <samtpomarico@gmail.com>
2020-11-12 19:01:47 -0600

    first commit on this branch. Completed 14.6.2 and 14.6.3

---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User(string username, string email, string password)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+        }
+    }
+}
